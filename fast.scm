6c0eacf558d3cfe56939b27f710f33594ac0cc41
omegatakuma <tooth0819@gmail.com>
2012-06-17 14:35:51 +0900

    up

---
+++ fast.scm
@@ -0,0 +1,33 @@
+#!/usr/local/bin/gosh
+
+(define count 0)
+
+(define (solve? num ls)
+  (let loop ((ls ls)(n 1))
+	(cond
+	  ((null? ls)#t)
+	  ((= (abs (- num (car ls))) n)
+	   #f)
+	  (else (loop (cdr ls)(+ n 1))))))
+
+(define (check? lst)
+  (let loop ((lst lst))
+	(cond
+	  ((null? (cdr lst))#t)
+	  ((solve? (car lst) (cdr lst))
+	   (loop (cdr lst)))
+	  (else #f))))
+
+(define (queen lst)
+  (let loop ((lst lst)(result '()))
+	(if (null? lst)
+		   (print (reverse result))
+	  (for-each (lambda(x)
+				  (if (check? (reverse (cons x result)))
+					(loop (delete x lst) (cons x result)))) lst))))
+  
+
+(define (main args)
+  (display ">> ")(flush)
+  (let ((n (read)))
+	(queen (iota n 0))))
